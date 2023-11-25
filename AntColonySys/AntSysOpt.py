4ed90f3034ef629c52e308600c6fd9cfc7c3f0d7
sfatew <tr.hoanganh2004@gmail.com>
2023-11-25 10:56:18 +0700

    update

--- AntColonySys/AntSysOpt.py
+++ AntColonySys/AntSysOpt.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 from io import StringIO
+import random
 import time
 
 with open("data5.txt", "r") as f:  
@@ -22,19 +23,39 @@ T=np.array(T)               #time travel matrix
 # print(c)
 # print(T)
 
-s=[0 for i in range(n+1)]  #solution
+s=[0 for i in range(n+1)]  #route
 
 t_i=0   # the time at which the salesman arrived at node i
 
 visited=[ False for i in range(n+1)]    # mark came points
 
-pheromone=np.full((n+1,n+1),0)           # matrix of pheromone at each arc
+pheromone=np.full((n+1,n+1),0.2)           # matrix of pheromone at each arc
 
 evaporation= 0.3                        # evaporation rate
 
-def pheromoneUpdate(i,j):
-    global pheromone
-    pheromone[i][j] = pheromone[i][j] * (1-evaporation) + 
+prob=np.array([0 for i in range(n+1)])          # prob to go from i to the next node
 
-def route_construction():
-    pass
+def pheromoneUpdate(i,j):
+
+    pheromone[i][j] = pheromone[i][j] * (1-evaporation) + delta_pheromone[i][j]
+
+def route_construction(k):        
+                                    #k : ant kth  (NOTE: ant k start at node k)
+    s[0]=k
+    visited[k]=True
+    for i in range(n):
+        if visited[i]==False:
+            pass
+
+def path_chosing(j):            #j : we are at the gth node of the route 
+    for i in range(n):
+        if visited[i]==False:
+            #update the prob of chosing the next node 
+            # alpha = 1, beta = 1
+            prob[i]= (pheromone**1 + (1/T[j][i])**1)     
+
+    prob=prob/sum(prob)
+    a=random.random()
+    
+
+
---
+++ testing.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a=np.array([1,2,3,4])
+
+a=a/sum(a)
+print(a)
