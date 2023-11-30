03fba74d873647eb317e51049c0a753f02d8a1a3
sfatew <130330044+sfatew@users.noreply.github.com>
2023-11-30 07:01:40 +0700

    update

--- AntColonySys/AntSysOpt.py
+++ AntColonySys/AntSysOpt.py
@@ -1,7 +1,6 @@
 import numpy as np
 import sys
 from io import StringIO
-import random
 import time
 
 with open("data5.txt", "r") as f:  
@@ -23,9 +22,7 @@ T=np.array(T)               #time travel matrix
 # print(c)
 # print(T)
 
-s=[0 for i in range(n+1)]  #route
-
-t_i=0   # the time at which the salesman arrived at node i
+route=[0 for i in range(n+1)]  #route
 
 visited=[ False for i in range(n+1)]    # mark came points
 
@@ -35,27 +32,51 @@ evaporation= 0.3                        # evaporation rate
 
 prob=np.array([0 for i in range(n+1)])          # prob to go from i to the next node
 
+delta_pheromone=np.zeros((n+1,n+1))
+
+timeTaken=0
+
+
 def pheromoneUpdate(i,j):
+    pheromone = pheromone * (1-evaporation) + delta_pheromone
 
-    pheromone[i][j] = pheromone[i][j] * (1-evaporation) + delta_pheromone[i][j]
+def route_construction(k,i):        
+                                    # 1st k : ant kth  (NOTE: ant k start at node k)
+                                    # later k : the current node
+                                    # i : interation(for recursion) 
+    global timeTaken
 
-def route_construction(k):        
-                                    #k : ant kth  (NOTE: ant k start at node k)
-    s[0]=k
     visited[k]=True
-    for i in range(n):
-        if visited[i]==False:
-            pass
+    route[i]=k
+    timeTaken=timeTaken + T[route[i-1]][route[i]] 
+    timeTaken=max(timeTaken,c[k][0])+ c[k][2]           # time that finished service at node k
+    
+    next=path_chosing(k)
+    if i!=n+1:
+        route_construction(next,i+1)
+
+
 
 def path_chosing(j):            #j : we are at the gth node of the route 
     for i in range(n):
         if visited[i]==False:
             #update the prob of chosing the next node 
             # alpha = 1, beta = 1
-            prob[i]= (pheromone**1 + (1/T[j][i])**1)     
-
+            prob[i]= (pheromone**1 * (1/T[j][i])**1)     
+    # try:
     prob=prob/sum(prob)
-    a=random.random()
+    next=np.random.choice(np.arange(0,(n+1)),p=prob)
+    prob=[0 for i in prob]
+    # except ZeroDivisionError:
+    #     next= route[0]
+
+    return next
+
+def AntSys():
+
+
+    while:
     
-
-
+        delta_pheromone=0
+        for k in range(n+1):
+            route_construction(k,0)
