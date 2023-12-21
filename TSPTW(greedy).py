b15db760737bba84dc6833de6db883e43c6b8324
sfatew <tr.hoanganh2004@gmail.com>
2023-12-21 19:34:20 +0700

    greedy

--- TSPTW(greedy).py
+++ TSPTW(greedy).py
@@ -4,63 +4,53 @@ from io import StringIO
 import time
 
 def greedy(T,c):
-   # find min route
-    sum = 0
-    counter = 0
-    j = 0
-    i = 0
-    min = 
- 
-    # Starting from the 0th indexed
-    # city i.e., the first city
-    visited[0] = 1
-    route = [0] * len(tsp)
- 
-    # Traverse the adjacency
-    # matrix tsp[][]
-    while i < len(tsp) and j < len(tsp[i]):
- 
-        # Corner of the Matrix
-        if counter >= len(tsp[i]) - 1:
-            break
- 
-        # If this path is unvisited then
-        # and if the cost is less then
-        # update the cost
-        if j != i and (visited[j] == 0):
-            if tsp[i][j] < min:
-                min = tsp[i][j]
-                route[counter] = j + 1
- 
-        j += 1
- 
-        # Check all paths from the
-        # ith indexed city
-        if j == len(tsp[i]):
-            sum += min
-            min = INT_MAX
-            visited[route[counter] - 1] = 1
-            j = 0
-            i = route[counter] - 1
-            counter += 1
- 
-    # Update the ending city in array
-    # from city which was last visited
-    i = route[counter - 1] - 1
- 
-    for j in range(len(tsp)):
- 
-        if (i != j) and tsp[i][j] < min:
-            min = tsp[i][j]
-            route[counter] = j + 1
- 
-    sum += min
- 
-    # Started from the node where
-    # we finished as well.
-    print("Minimum Cost is :", sum)
- 
+    '''travel from i to a node j
+    node j define by choseing the node with the lowest start serving time'''
 
+    global time_taken
+
+    visited=[ 0 for i in range(n+1)]    # mark came points
+
+    t_i=0   # the time at which the salesman arrived at node i
+
+    t1_i=0           # the time at which the salesman start service at node i
+
+    counter = 0        # number of city we have visited
+
+    next=0          # the next node chosen by greedy
+
+    # Starting from the 0th indexed city i.e., the first city
+    visited[0] = 1
+
+    while True:
+        min = np.inf
+
+        counter+=1
+        a=[0]
+        if counter > n:
+            break
+
+        for j in range(1,n+1):
+            # print(t1_i) 
+            a[0]=t1_i
+            if visited[j]==0:       
+                t_i= a[0]+ c[route[counter-1]][2]+ T[route[counter-1]][j]   
+                a[0]= max(c[j][0], t_i)     
+                # print(a[0])
+                if a[0] > c[j][1]:
+                    raise ValueError('exceed time limit at node',j)
+
+                if a[0] < min:
+                    min = a[0]
+                    next=j
+                    # print(next)
+   
+        visited[next] = 1
+        route[counter] = next
+        t1_i = min
+        time_taken = t1_i
+
+    time_taken = time_taken + c[route[-1]][2] + T[route[-1]][0]
 
 if __name__ == '__main__':
     with open("data/data5.txt", "r") as f:  
@@ -79,5 +69,13 @@ if __name__ == '__main__':
         T.append(time_travel) 
     T=np.array(T)               #time travel matrix
 
+    route = [0 for i in range(n+1)]
+    time_taken = 0
 
-    visited=[ 0 for i in range(n+1)]    # mark came points
+    begin=time.time()
+    greedy(T,c)
+    print(route)
+    print(time_taken)
+    finish=time.time()
+
+    print('time taken', finish-begin)
